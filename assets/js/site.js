df0bd5ab7f84dedb03128800facf4f858d2a5f28
OmidDeiminiyat <omid.deiminiyat@yahoo.com>
2024-03-04 10:08:43 +0100

    Opgave CSS-Training

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,43 @@
 // write cool JS hwere!!
+
+
+
+
+const firstItem = document.getElementById('No-one').value
+const secondItem = document.getElementById('No2').value
+const thirdItem = document.getElementById('No3').value
+
+
+
+
+function validateForm() {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    var checkedItems = [];
+
+    checkboxes.forEach(function(checkbox) {
+        if (checkbox.checked) {
+            checkedItems.push(checkbox.value);
+        }
+    });
+
+    if (checkedItems.length > 0) {
+       // console.log('Checked item(s):', checkedItems.join(', '));
+        alert('You checked:' + checkedItems)
+    } else {
+        alert('No item checked.')
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
